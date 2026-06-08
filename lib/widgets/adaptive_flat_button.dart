6d1e1bbc1175e72a1d30a69c5ed7611aeb6dc9f8
Boiyee <odjubreel2@gmail.com>
2026-06-08 03:18:12 +0100

    naming correction

---
+++ lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  const AdaptiveFlatButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
