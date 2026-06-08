a4d2034d706979d422b4cea0da2be71c8aca1590
Boiyee <odjubreel2@gmail.com>
2026-06-08 03:35:13 +0100

    wrap up

--- lib/widgets/adaptive_flat_button.dart
+++ lib/widgets/adaptive_flat_button.dart
@@ -1,11 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class AdaptiveFlatButton extends StatelessWidget {
-  const AdaptiveFlatButton({super.key});
+  final String text;
+  final VoidCallback handler;
+  AdaptiveFlatButton(this.text, this.handler);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Platform.isIOS
+        ? CupertinoButton(child: Text(text), onPressed: handler)
+        : TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Theme.of(context).primaryColor,
+            ),
+            child: Text(text, style: TextStyle(fontWeight: FontWeight.bold)),
+            onPressed: handler,
+          );
   }
 }
